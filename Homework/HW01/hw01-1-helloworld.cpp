d067d940fdc5430f39f67423a84dc6420c5fc2ba
teeratapdeesussus <deesussus999@gmail.com>
2023-08-14 00:01:15 +0700

    hw1-1

--- Homework/HW01/hw01-1-helloworld.cpp
+++ Homework/HW01/hw01-1-helloworld.cpp
@@ -1,9 +1,6 @@
 #include <stdio.h>
 int main () {
-    int a=10 ; //TODO: #3 provide me full syntax of C-Language contain include, main function(int).
-    printf("Hello World") ; //TODO: #4 display me "Hello World." to me.
-    scanf("%d",&a) ; //TODO: #5 Write your private info into Student-Info.txt
-    printf("a=%d,a") ;
+    printf("Hello World") ;
     
     return 0 ;
 }//end main function
