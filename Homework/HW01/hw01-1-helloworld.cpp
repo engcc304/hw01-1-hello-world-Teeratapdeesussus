dd605d2900c4bc8fa7999e3ca780a40f31776080
teeratapdeesussus <deesussus999@gmail.com>
2023-08-13 20:21:36 +0700

    hw1

--- Homework/HW01/hw01-1-helloworld.cpp
+++ Homework/HW01/hw01-1-helloworld.cpp
@@ -1,5 +1,10 @@
-//TODO: #3 provide me full syntax of C-Language contain include, main function(int).
-
-//TODO: #4 display me "Hello World." to me.
-
-//TODO: #5 Write your private info into Student-Info.txt
+#include <stdio.h>
+int main () {
+    int a=10 ; //TODO: #3 provide me full syntax of C-Language contain include, main function(int).
+    printf("Hello World") ; //TODO: #4 display me "Hello World." to me.
+    scanf("%d",&a) ; //TODO: #5 Write your private info into Student-Info.txt
+    printf("a=%d,a") ;
+    
+  
+    return 0 ;
+}//end main function
